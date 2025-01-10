c538001f74f0aa616d6a8699930b9fd51825fe26
Nyankoye Daniel FELEMOU <nyankoyedanielfelemou32156@gmail.com>
2025-01-10 12:48:38 +0000

    Apperçus général de l'objet ORM Doctrine de Symfony, sur les methodes de manipulation des données de base comme findAll(),findOneBySlug(), findOneBy(), find() etc.. et aussi un petit apperçu sur le CRUD dont les methods ou actions doivente être persiter par la methods persits() ou flush()

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
 # TutoSymfony
 Cours de symfony avec quelque explications
+
+## Pour creer une entity 
+*php bin/console make:entity 'entity_name'*
+dire par la suite *non*
+Et ensuite on enumere les properties avec les datas types
+
+## Apres tous il faudrais taper la commande pour pouvoir creer les fichiers:
+*php bin/console make:migration*
+
+## Pour la mise a jour de notre migration : 
+il faut taper la commande : 
+*php bin/console doctrine:migrations:migrate*
+
+
+## Pour modifier une entity il faut taper la commande : 
+*php bin/console make:entiry 'entity_name'*
+
+Et apres avoir creer un nouveau champ ou colonnes il faut donc taper la commande :
+*php bin/console make:migration*
+
+Et en suite migrer : *php bin/console doctrine:migrations:migrate*
+
+### Faire une recherche sur les query builder de symfony
+
+<!-- Pour la modification, l'ajout la suppressio d'une donnees dans la table il faut utiliser le entity manager interface dans le controlleur de l'entite et a la fin de chaque methode a savoir  -->
+
+*Pour l'ajout il faut donc faire de la sorte :*
+ <!-- Creation d'une nouvelle donnees (recipe)
+ $recipe = new Recipe();
+ $recipe->setTitle('Barbe à papa')
+ ->setSlug('barbe-a-papa')
+ ->setContent('Mettez du sucre')
+ ->setDuration(15)
+ ->setCreatedAt(new \DateTimeImmutable())
+ ->setUpdatedAt(new \DateTimeImmutable());
+ $em->persist($recipe);
+ $em->flush(); -->
+
+*Pour la modificaiton il faut donc appeler la methode "flush()" pour que les m-a-j soient garder dans la database*
+<!-- $recipes[0]->setTitle("Mousse au Chocolat");
+$em->flush(); -->
+
+*Si l'on souhaite maintenant supprimer une donnees dans la base des donnees il faut :*
+<!-- $em->remove($recipes[0]);
+$em->flush(); -->
+
--- src/Controller/RecipeController.php
+++ src/Controller/RecipeController.php
@@ -2,11 +2,14 @@
 
 namespace App\Controller;
 
+use App\Entity\Recipe;
+use App\Repository\RecipeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Doctrine\ORM\EntityManagerInterface;
 
 class RecipeController extends AbstractController
 {
@@ -24,17 +27,42 @@ class RecipeController extends AbstractController
     //     dd($slug, $id);
     // }
 
-    #[Route('/recettes', name:'recipe.index')]
-    public function index(Request $request): Response{
+    #[Route('/recettes', name: 'recipe.index')]
+    public function index(Request $request, RecipeRepository $repository, EntityManagerInterface $em): Response
+    {
         // return new Response("Recettes");
-        return $this->render('recipe/index.html.twig');
+        // $recipes = $repository->findAll();
+        // $recipes =  $repository->findWithDurationLowerThan(100);
+        // $recipes = $em->getRepository(Recipe::class)->findWithDurationLowerThan(100);
+
+        // Pour la modificaiton il faut donc appeler la methode flush() pour que les m-a-j soient garder dans la database
+        // $recipes[0]->setTitle("Mousse au Chocolat");
+
+        // Creation d'une nouvelle donnees (recipe)
+        // $recipe = new Recipe();
+        // $recipe->setTitle('Barbe à papa')
+        // ->setSlug('barbe-a-papa')
+        // ->setContent('Mettez du sucre')
+        // ->setDuration(15)
+        // ->setCreatedAt(new \DateTimeImmutable())
+        // ->setUpdatedAt(new \DateTimeImmutable());
+        // $em->persist($recipe);
+
+        // Si l'on souhaite maintenant supprimer une donnees dans la base des donnees il faut : 
+        // $em->remove($recipes[0]);
+        // $em->flush();
+
+        /** Au cas nous voulons faire des requettes un peut plus differentes */
+        // dd($repository->findTotalDuration());
+        $recipes = $repository->findWithDurationLowerThan(50);
+        return $this->render('recipe/index.html.twig', ['recipes' => $recipes]);
     }
 
-    #[Route('/recettes/{slug}-{id}', name:'recipe.show', requirements: ['id' => '\d+', 'slug' => '[a-z0-9-]+'])]
-    public function show(Request $request, string $slug, int $id): Response
+    #[Route('/recettes/{slug}-{id}', name: 'recipe.show', requirements: ['id' => '\d+', 'slug' => '[a-z0-9-]+'])]
+    public function show(Request $request, string $slug, int $id, RecipeRepository $repository): Response
     {
         // return new Response("Recette : ". $slug);
-        
+
         // return new JsonResponse([
         //     'id' => $id,
         //     'slug' => $slug,
@@ -46,14 +74,37 @@ class RecipeController extends AbstractController
         // ]);
 
 
+        // return $this->render('recipe/show.html.twig', [
+        //     'id' => $id,
+        //     'slug' => $slug,
+        //     'person' => [
+        //         'firstname' => 'Nyankoye Daniel',
+        //         'lastname' => 'FELEMOU',
+        //         'age' => 30,
+        //     ]
+        // ]);
+
+        /**
+         * On peut essayer plusieur maniere en faissant par 
+         * $repository->findOneBySlug($slug);
+         * $repository->find($id);
+         * $repository->findOneBy(['slug' => $slug]);
+         * $repository->findOneBySlug($slug);
+         */
+
+        // $recipe = $repository->findOneBy(['slug' => $slug]);
+        $recipe = $repository->find($id);
+
+        if ($recipe->getSlug() !== $slug) {
+            return $this->redirectToRoute('recipe.show',
+            [
+                'slug' => $recipe->getSlug(),
+                'id' => $recipe->getId()
+            ]);
+        }
+
         return $this->render('recipe/show.html.twig', [
-            'id' => $id,
-            'slug' => $slug,
-            'person' => [
-                'firstname' => 'Nyankoye Daniel',
-                'lastname' => 'FELEMOU',
-                'age' => 30,
-            ]
+            'id' => $id, 'recipe' => $recipe
         ]);
     }
 }
